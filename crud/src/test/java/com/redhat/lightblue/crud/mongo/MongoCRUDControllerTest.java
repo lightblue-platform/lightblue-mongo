c0b5e56d5cdb6f0f38becdccae9ebac871908cf5
Burak Serdar <bserdar@redhat.com>
2015-10-06 10:21:11 -0600

    Add unit test with an in query

--- crud/src/test/java/com/redhat/lightblue/crud/mongo/MongoCRUDControllerTest.java
+++ crud/src/test/java/com/redhat/lightblue/crud/mongo/MongoCRUDControllerTest.java
@@ -587,6 +587,33 @@ public class MongoCRUDControllerTest extends AbstractMongoCrudTest {
     }
 
     @Test
+    public void findInq() throws Exception {
+        EntityMetadata md = getMd("./testMetadata.json");
+        TestCRUDOperationContext ctx = new TestCRUDOperationContext(CRUDOperation.INSERT);
+
+        ctx.add(md);
+        // Generate some docs
+        List<JsonDoc> docs = new ArrayList<>();
+        int numDocs = 20;
+        for (int i = 0; i < numDocs; i++) {
+            JsonDoc doc = new JsonDoc(loadJsonNode("./testdata1.json"));
+            doc.modify(new Path("field1"), nodeFactory.textNode("doc" + i), false);
+            doc.modify(new Path("field3"), nodeFactory.numberNode(i), false);
+            doc.modify(new Path("field4"), nodeFactory.numberNode(new BigDecimal(100)),false);
+            docs.add(doc);
+        }
+        ctx.addDocuments(docs);
+        controller.insert(ctx, projection("{'field':'_id'}"));
+
+        ctx = new TestCRUDOperationContext(CRUDOperation.FIND);
+        ctx.add(md);
+        controller.find(ctx,query("{'field':'field4','op':'$in','values':[100]}"),
+                        projection("{'field':'*','recursive':1}"),null,null,null);
+        Assert.assertEquals(numDocs,ctx.getDocuments().size());
+
+    }
+
+    @Test
     public void sortAndPageTest() throws Exception {
         EntityMetadata md = getMd("./testMetadata.json");
         TestCRUDOperationContext ctx = new TestCRUDOperationContext(CRUDOperation.INSERT);
