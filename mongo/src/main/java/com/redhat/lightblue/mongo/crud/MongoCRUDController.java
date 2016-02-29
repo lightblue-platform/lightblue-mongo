459f904493e1936f0cca0f4e303c8275d97494d7
bvulaj <bvulaj@redhat.com>
2016-02-29 15:57:28 -0500

    Refactor to break population out into its own method

--- mongo/src/main/java/com/redhat/lightblue/mongo/crud/MongoCRUDController.java
+++ mongo/src/main/java/com/redhat/lightblue/mongo/crud/MongoCRUDController.java
@@ -53,6 +53,8 @@ import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -709,21 +711,7 @@ public class MongoCRUDController implements CRUDController, MetadataListener, Ex
                 // if we're not running in the background, we need to execute the items in this block afterwards
                 // caseInsensitive indexes have been updated or created, run the server-side updater on mongo to recalculate all hidden fields
 
-                String js = new String(Files.readAllBytes(Paths.get(ClassLoader.getSystemResource("js/populate-hidden-fields.js").toURI())));
-
-                // Use the newer Mongo objects here
-                BsonDocument popHiddenFieldsFunction = new BsonDocument("value", new BsonJavaScript(js));
-                MongoConfiguration config = dbResolver.getConfiguration(ds);
-                MongoDatabase mdb = config.getMongoClient().getDatabase(ds.getDatabaseName());
-
-                mdb.getCollection("system.js").updateOne(
-                        new Document("_id", "popHiddenFieldsFunction"),
-                        new Document("$set", popHiddenFieldsFunction),
-                        new UpdateOptions().upsert(true));
-
-                mdb.runCommand(new Document("$eval", "db.loadServerScripts()"));
-
-                mdb.runCommand(new Document("$eval", "popHiddenFieldsFunction(" + fieldMap + ")"));
+                populateHiddenFields(ei, fieldMap);
 
                 // TODO: remove hidden fields on index deletions? Worth it?
             }
@@ -739,6 +727,26 @@ public class MongoCRUDController implements CRUDController, MetadataListener, Ex
         }
 
         LOGGER.debug("createUpdateEntityInfoIndexes: end");
+    }
+
+    private void populateHiddenFields(EntityInfo ei, Map<String, String> fieldMap) throws IOException, URISyntaxException {
+        MongoDataStore ds = (MongoDataStore) ei.getDataStore();
+
+        String js = new String(Files.readAllBytes(Paths.get(ClassLoader.getSystemResource("js/populate-hidden-fields.js").toURI())));
+
+        // Use the newer Mongo objects here
+        BsonDocument popHiddenFieldsFunction = new BsonDocument("value", new BsonJavaScript(js));
+        MongoConfiguration config = dbResolver.getConfiguration(ds);
+        MongoDatabase mdb = config.getMongoClient().getDatabase(ds.getDatabaseName());
+
+        mdb.getCollection("system.js").updateOne(
+                new Document("_id", "popHiddenFieldsFunction"),
+                new Document("$set", popHiddenFieldsFunction),
+                new UpdateOptions().upsert(true));
+
+        mdb.runCommand(new Document("$eval", "db.loadServerScripts()"));
+
+        mdb.runCommand(new Document("$eval", "popHiddenFieldsFunction(" + fieldMap + ")"));
     }
 
     private DBObject findIndexWithSignature(List<DBObject> existingIndexes,Index index) {
