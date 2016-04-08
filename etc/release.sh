dbc9ef1e2e0e6d390b67bce4bf1eaf3c4151a024
bvulaj <bvulaj@redhat.com>
2016-04-08 14:19:13 -0400

    Updated script to update individual versions as well as props.  Also allowed including / excluding

--- etc/release.sh
+++ etc/release.sh
@@ -4,6 +4,8 @@
 
 RELEASE_VERSION=$1
 DEVEL_VERSION=$2
+INCLUDES_DEPS=$3
+EXCLUDES_DEPS=$4
 
 if [ $1"x" == "x" ] || [ $2"x" == "x" ]; then
     echo "Usage: ./release.sh <release version> <new snapshot version>"
@@ -34,7 +36,8 @@ if [ $MERGE_BASE != $HEAD_HASH ]; then
 fi
 
 # update to non-snapshot versions of lightblue dependencies and commit
-mvn versions:update-properties -DallowSnapshots=false
+# excludes overrides includes, so to exclude com.redhat.lightblue, add it to excludes
+mvn versions:use-latest-versions versions:update-properties -Dincludes=com.redhat.lightblue:*,INCLUDES_DEPS -Dexcludes=EXCLUDES_DEPS
 git commit -a -m "Updated versions to non snapshot"
 
 # prepare for release (note, this will warn if any snapshot dependencies still exist and allow for fixing)
@@ -51,7 +54,7 @@ git push origin master --tags
 mvn release:perform -P release | tee release.log || exit
 
 # update to latest lightblue snapshot dependencies
-mvn versions:use-latest-snapshots versions:update-properties -Dincludes=*lightblue* -DallowSnapshots=true
+mvn versions:use-latest-snapshots versions:update-properties -DallowSnapshots=true -Dincludes=com.redhat.lightblue:*,INCLUDES_DEPS -Dexcludes=EXCLUDES_DEPS
 git add pom.xml **/pom.xml
 git commit -m "Updated to latest snapshot dependencies"
 git push origin master
